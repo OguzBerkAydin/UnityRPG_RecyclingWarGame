5b2ccc4a05f8592ac72ca3ab52b1185af360134a
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2023-06-19 01:43:54 +0300

    Reycle objects added and some scripts written

---
+++ Assets/DeadZone.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(player);
+        }
+    }
+}
